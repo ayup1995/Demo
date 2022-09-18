a59cc2dbbd4e7921cdc89bb3d398c5ed49826dd8
Ayushi <ayupar1995@gmail.com>
2022-09-18 12:43:59 +0530

    first commit

---
+++ Middleware/RequestQueryStringMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace introAspNetCore.Middleware
+{
+    public class RequestQueryStringMiddleware
+    {
+        RequestDelegate next;
+        public RequestQueryStringMiddleware(RequestDelegate nextDelegate)
+        {
+            next = nextDelegate;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Get
+                && context.Request.Query["iscertifoed"] == "true")
+            {
+                await context.Response.WriteAsync("message from class-based Middleware \n");
+            }
+            await next(context);
+        }
+    }
+}
